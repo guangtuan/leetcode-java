796992367d6d56a677b1311abe81bac5b7c90bb6
guangtuan <zhuangguangtuan@gmail.com>
2022-05-18 22:05:19 +0800

    common: refactor: 更激进一些

--- src/main/java/tech/igrant/binarySearch/no704/Solution.java
+++ src/main/java/tech/igrant/binarySearch/no704/Solution.java
@@ -5,19 +5,16 @@ class Solution {
     // [0,3] m = 1
     // [0,0], [2,3]
     public int search(int[] nums, int target) {
-        return search(nums, 0, nums.length - 1, target);
+        return search(nums, target, 0, nums.length - 1, nums.length / 2);
     }
 
-    private int search(int[] nums, int left, int right, int target) {
-        if (left > right) {
-            return -1;
-        }
-        int middle = (right - left) / 2 + left;
-        return nums[middle] == target ?
-                middle :
-                nums[middle] < target ?
-                        search(nums, middle + 1, right, target) :
-                        search(nums, left, middle - 1, target);
+    private int search(int[] nums, int target, int left, int right, int mid) {
+        return left > right ? -1 :
+                nums[mid] == target ?
+                        mid :
+                        nums[mid] < target ?
+                                search(nums, target, mid + 1, right, (mid + 1 + right) / 2) :
+                                search(nums, target, left, mid - 1, (left + mid - 1) / 2);
     }
 
 }
