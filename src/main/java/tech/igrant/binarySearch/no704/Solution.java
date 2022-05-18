0c56f86fbd71c4ab44afe4f8272f598e9aff2aaa
guangtuan <zhuangguangtuan@gmail.com>
2022-05-18 22:22:43 +0800

    common: refactor #704，就酱吧

--- src/main/java/tech/igrant/binarySearch/no704/Solution.java
+++ src/main/java/tech/igrant/binarySearch/no704/Solution.java
@@ -5,16 +5,23 @@ class Solution {
     // [0,3] m = 1
     // [0,0], [2,3]
     public int search(int[] nums, int target) {
-        return search(nums, target, 0, nums.length - 1, nums.length / 2);
+        QuadFunction quadFunction = new QuadFunction() {
+            public int search(int[] nums, int target, int left, int right) {
+                int mid = (right - left) / 2 + left;
+                return left <= right ?
+                        nums[mid] != target ?
+                                nums[mid] < target ?
+                                        search(nums, target, mid + 1, right) :
+                                        search(nums, target, left, mid - 1)
+                                : mid
+                        : -1;
+            }
+        };
+        return quadFunction.search(nums, target, 0, nums.length - 1);
     }
 
-    private int search(int[] nums, int target, int left, int right, int mid) {
-        return left > right ? -1 :
-                nums[mid] == target ?
-                        mid :
-                        nums[mid] < target ?
-                                search(nums, target, mid + 1, right, (mid + 1 + right) / 2) :
-                                search(nums, target, left, mid - 1, (left + mid - 1) / 2);
+    private interface QuadFunction {
+        int search(int[] nums, int target, int left, int right);
     }
 
 }
