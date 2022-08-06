c5498190a981e2e8aab7f975b4f964868499f71f
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-08-06 18:41:23 +0800

    refactor: 重构第三题的写法

--- src/main/java/tech/igrant/no3/Solution.kt
+++ src/main/java/tech/igrant/no3/Solution.kt
@@ -1,20 +1,24 @@
 package tech.igrant.no3
 
+
 class Solution {
 
     fun lengthOfLongestSubstring(s: String): Int {
-        // 双指针，找 i, j 范围不重复，维护一个 set
-        val st = HashSet<Char>()
+        val map = mutableMapOf<Char, Int>()
+        // 左指针应该设置为 -1，这样子当 index = 0 的时候，区间范围的大小其实是 0 - (-1) = 1
+        var left = 0
         var max = 0
-        for (i in s.indices) {
-            for (j in i until s.length) {
-                if (!st.add(s[j])) {
-                    max = max.coerceAtLeast(st.size)
-                    st.clear()
-                }
+        for (right in s.indices) {
+            val curr = s[right]
+            // 使用 map 检测右侧值是否出现过
+            map[curr]?.let { indexWhereRepeat ->
+                // 如果出现过，将左指针向右移动，到上一次出现右值的位置（如果这个位置比左指针小，则不移动）
+                left = left.coerceAtLeast(indexWhereRepeat + 1)
             }
-            max = max.coerceAtLeast(st.size)
-            st.clear()
+            // 将右指针和右侧值放入 map
+            map[curr] = right
+            // 同时计算最长的区间范围
+            max = max.coerceAtLeast(right - left + 1)
         }
         return max
     }
--- src/test/java/tech/igrant/no3/SolutionTest.kt
+++ src/test/java/tech/igrant/no3/SolutionTest.kt
@@ -8,8 +8,10 @@ class SolutionTest {
     data class TestCase(val s: String, val exp: Int)
 
     val testCases = listOf(
-        TestCase(s = "jbpnbwwd", exp = 4),
         TestCase(s = " ", exp = 1),
+        TestCase(s = "jbpnbwwd", exp = 4),
+        TestCase(s = "abba", exp = 2),
+        TestCase(s = "", exp = 0),
         TestCase(s = "abcabcbb", exp = 3),
         TestCase(s = "pwwkew", exp = 3),
         TestCase(s = "bbbbb", exp = 1)
@@ -19,7 +21,14 @@ class SolutionTest {
 
     @Test
     fun lengthOfLongestSubstring() {
+        // 输入 p，结果 p
+        // 输入 w，结果 p,w
+        // 输入 w，结果 w
+        // 输入 k，结果 w,k
+        // 输入 e，结果 w,k,e
+        // 输入 w，结果 k,e,w
         for (testCase in testCases) {
+            println(testCase)
             assertEquals(
                 testCase.exp,
                 solution.lengthOfLongestSubstring(testCase.s)
