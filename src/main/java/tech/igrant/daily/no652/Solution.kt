044654c2c391230489ebc80ff91d972e40d75b97
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-05 22:40:51 +0800

    daily -> no652 refactor

--- src/main/java/tech/igrant/daily/no652/Solution.kt
+++ src/main/java/tech/igrant/daily/no652/Solution.kt
@@ -7,28 +7,31 @@ class Solution {
     // 遍历所有的节点，得到值，去重后给出答案
 
     fun findDuplicateSubtrees(root: TreeNode?): List<TreeNode?> {
+        val st = mutableMapOf<String, Int>()
         return root?.let {
             val found = mutableListOf<TreeNode>()
-            val ans = mutableListOf<String>()
-            bfs(root, ans, found)
-            val st = ans.foldIndexed(mutableMapOf()) { index, acc: MutableMap<String, MutableList<Int>>, s ->
-                acc[s]?.add(index) ?: run { acc[s] = mutableListOf(index) }
-                acc
-            }
-            println(st.keys)
-            st.filterValues { it.size > 1 }.map { it.value[0] }.map { found[it] }
+            bfs(root, st, found)
+            found
         } ?: emptyList()
     }
 
-    private fun bfs(root: TreeNode, cache: MutableList<String>, found: MutableList<TreeNode>): String {
+    private fun bfs(root: TreeNode, cache: MutableMap<String, Int>, found: MutableList<TreeNode>): String {
         val v = "${root.`val`}"
         val l = root.left?.let { bfs(it, cache, found) }
         val r = root.right?.let { bfs(it, cache, found) }
-        val lStr = l?.let { "l$it" } ?: "ln"
-        val rStr = r?.let { "r$it" } ?: "rn"
-        val ret = listOf(v, lStr, rStr).joinToString(":")
-        cache.add(ret)
-        found.add(root)
+        val ret = listOf(
+            v,
+            l?.let { "l$it" } ?: "ln",
+            r?.let { "r$it" } ?: "rn"
+        ).joinToString(":")
+        cache[ret]?.let {
+            if (it != 2) {
+                found.add(root)
+                cache[ret] = 2
+            }
+        } ?: run {
+            cache[ret] = 1
+        }
         return ret
     }
 
