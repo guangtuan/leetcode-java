6cfc03558d74951d43ce94f8091c488c0d752258
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-08 22:54:01 +0800

    daily -> no667 refactor 换一种思路

--- src/main/java/tech/igrant/daily/no667/Solution.kt
+++ src/main/java/tech/igrant/daily/no667/Solution.kt
@@ -3,29 +3,34 @@ package tech.igrant.daily.no667
 class Solution {
 
     fun constructArray(n: Int, k: Int): IntArray {
+        // 10, 6 -> [1, (+6=)7, (-5=)2, (+4=)6, (-3=)3, (+2=)5, (-1=)4, 8,9,10]
+        // 3, 2 -> [1, (+2=)3, (-1=)2]
+
         // 1,2,3,4,5,6,7,8,9,10
-        // 10, 7 -> [1,7,2,6,3,5,4,8,9,10]
+        // 3, 2 -> [1, 3, 2]
+        //            2  1
+        // 3, 1 -> [1,2,3]
+        // 8, 7 -> [1, 8, 2, 7, 3, 6, 4, 5]
+        //            7
         // 10, 5 -> [1,6,2,5,3,4,7,8,9,10]
         // 10, 4 -> [1,5,2,4,3,6,7,8,9,10]
         // 10, 3 -> [1,4,2,3,5,6,7,8,9,10]
-        val ret = mutableListOf<Int>()
-        var i = 1
-        var j = 1 + k
-        while (true) {
-            ret.add(i++)
-            ret.add(j--)
-            if (i == j) {
-                ret.add(i)
-                break
-            } else if (i > j) {
-                break
-            }
+        val ret = mutableListOf<Int>().also { it.add(1) }
+        var init = k
+        var flag = 1
+        while (init != 0) {
+            ret.add(last(ret) + flag * init)
+            flag *= -1
+            init -= 1
         }
-        var end = 1 + k + 1
+        var init2 = k + 2
         while (ret.size < n) {
-            ret.add(end++)
+            ret.add(init2)
+            init2 += 1
         }
         return ret.toIntArray()
     }
+
+    private fun last(ret: MutableList<Int>) = ret[ret.size - 1]
 
 }
--- src/test/java/tech/igrant/daily/no667/SolutionTest.kt
+++ src/test/java/tech/igrant/daily/no667/SolutionTest.kt
@@ -2,13 +2,17 @@ package tech.igrant.daily.no667
 
 import org.junit.Test
 import kotlin.test.assertContentEquals
-import kotlin.test.assertEquals
 
 class SolutionTest {
 
     private class TestCase(val n: Int, val k: Int, val exp: IntArray)
 
     private val ts: List<TestCase> = listOf(
+        TestCase(
+            n = 24,
+            k = 4,
+            intArrayOf(1, 5, 2, 4, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
+        ),
         TestCase(n = 8, k = 7, exp = intArrayOf(1, 8, 2, 7, 3, 6, 4, 5)),
         TestCase(n = 3, k = 1, exp = intArrayOf(1, 2, 3)),
         TestCase(n = 3, k = 2, exp = intArrayOf(1, 3, 2))
