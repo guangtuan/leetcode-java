e1e541af24a5ef41304a7ebffe06e41933b8b335
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-25 16:57:27 +0800

    weekly_contest -> no312 refactor no1
    原来 kt 也有 zip 操作

--- src/main/java/tech/igrant/weekly_contest/no312/no1/Solution.kt
+++ src/main/java/tech/igrant/weekly_contest/no312/no1/Solution.kt
@@ -1,9 +1,7 @@
 package tech.igrant.weekly_contest.no312.no1
 
 class Solution {
-    fun sortPeople(names: Array<String>, heights: IntArray): Array<String> {
-        val sortedIndex = heights.mapIndexed { i, h -> Pair(i, h) }.sortedByDescending { it.second }.map { it.first }
-        val lookupByIndex = names.mapIndexed { i, n -> Pair(i, n) }.associateBy { it.first }
-        return sortedIndex.mapNotNull { lookupByIndex[it] }.map { it.second }.toTypedArray()
-    }
+    fun sortPeople(names: Array<String>, heights: IntArray): Array<String> =
+            heights.toTypedArray().zip(names).sortedByDescending { it.first }.map { it.second }.toTypedArray()
+
 }
