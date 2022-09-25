c1d2209b6acd15000962463bbd4a4b3523b780b3
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-09-25 16:50:14 +0800

    weekly_contest -> no312 -> refactor no3
    重构写法
    
    利用单调性计算前缀和

--- src/main/java/tech/igrant/weekly_contest/no312/no3/Solution.kt
+++ src/main/java/tech/igrant/weekly_contest/no312/no3/Solution.kt
@@ -3,55 +3,21 @@ package tech.igrant.weekly_contest.no312.no3
 class Solution {
 
     fun goodIndices(nums: IntArray, k: Int): List<Int> {
-        val notIncrease = BooleanArray(nums.size) { false }
-        val notDecrease = BooleanArray(nums.size) { true }
-        for (i in 0..nums.size - 2) {
-            notIncrease[i] = notIncrease(nums[i], nums[i + 1])
-        }
-        for (i in 1 until nums.size) {
-            notDecrease[i] = notDecrease(nums[i - 1], nums[i])
-        }
-        val rangeStart = k
-        val rangeEnd = nums.size - k - 1
-        val ans = mutableListOf<Int>()
-        for (i in rangeStart..rangeEnd) {
-            val preStart = i - k;
-            val preEnd = i - 1;
-            val postStart = i + 1;
-            val postEnd = i + k;
-            if (notInc(notIncrease, preStart, preEnd)) {
-                if (notDec(notDecrease, postStart, postEnd)) {
-                    ans.add(i)
-                }
+        // 正序：算出每一个位置（包含自身）前的非递增子数组长度
+        // 倒序：算出每一个位置（包含自身）后的非递减子数组长度
+        val noneInc = IntArray(nums.size) { 1 }
+        for (i in 1..nums.size - k) {
+            if (nums[i] <= nums[i - 1]) {
+                noneInc[i] = noneInc[i - 1] + 1
             }
         }
-        return ans
-    }
-
-    private fun notDec(notDecrease: BooleanArray, postStart: Int, postEnd: Int): Boolean {
-        for (i in postStart + 1..postEnd) {
-            if (!notDecrease[i]) {
-                return false
+        val noneDec = IntArray(nums.size) { 1 }
+        for (i in nums.size - 2 downTo k) {
+            if (nums[i] <= nums[i + 1]) {
+                noneDec[i] = noneDec[i + 1] + 1
             }
         }
-        return true
-    }
-
-    private fun notInc(notIncrease: BooleanArray, preStart: Int, preEnd: Int): Boolean {
-        for (i in preStart until preEnd) {
-            if (!notIncrease[i]) {
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun notIncrease(n1: Int, n2: Int): Boolean {
-        return n1 >= n2
-    }
-
-    private fun notDecrease(n1: Int, n2: Int): Boolean {
-        return n1 <= n2
+        return IntArray(nums.size - k - k) { it + k }.filter { i -> noneInc[i - 1] >= k && noneDec[i + 1] >= k }
     }
 
 }
