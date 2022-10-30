bf8d0bb585108fb551cc05b775a57bbef627f567
zhuangguangtuan <zhuangguangtuan@gmail.com>
2022-10-30 20:46:26 +0800

    weekly contest#317 -> no3 refactor
    重构成不可变的写法

--- src/main/java/tech/igrant/weekly_contest/no317/no3/Solution.kt
+++ src/main/java/tech/igrant/weekly_contest/no317/no3/Solution.kt
@@ -4,47 +4,40 @@ import kotlin.math.pow
 
 class Solution {
 
-    fun parseNumber(n: Long): MutableList<Int> {
-        var remain = n
-        val ret = mutableListOf<Int>()
-        while (remain > 0) {
-            val curr = remain % 10
-            remain /= 10
-            ret.add(0, curr.toInt())
+    private fun numberToIntList(n: Long): List<Int> {
+        if (n < 10) {
+            return listOf(n.toInt())
         }
-        return ret
+        val curr = n % 10
+        return numberToIntList(n / 10).toMutableList().apply { this.add(curr.toInt()) }.toList()
     }
 
-    private fun sum(l: List<Int>): Int {
-        var ret = 0
-        l.forEach { ret += it }
-        return ret
-    }
+    fun makeIntegerBeautiful(n: Long, target: Int): Long = numberToIntList(n)
+            .let { p ->
+                toNumber(doRec(target, p, p.size - 1)) - n
+            }
 
-    fun makeIntegerBeautiful(n: Long, target: Int): Long {
-        val parsed = parseNumber(n)
-        var s = parsed.sum()
-        var cursor = parsed.size - 1
-        while (s > target) {
-            inc(parsed, cursor)
-            s = sum(parsed)
-            cursor -= 1
+    private fun doRec(target: Int, bits: List<Int>, cursor: Int): List<Int> {
+        return when {
+            bits.sum() <= target -> bits
+            else -> doRec(target, increase(bits, cursor), cursor.dec())
         }
-        val ret = toNumber(parsed)
-        println("ret is $ret")
-        return ret - n
     }
 
-    private fun toNumber(l: List<Int>): Long {
-        return l.mapIndexed { index, i -> (i * (10.toDouble().pow((l.size - index - 1).toDouble()))).toLong() }.sum()
-    }
+    private fun toNumber(l: List<Int>): Long =
+            l.mapIndexed { index, i -> (i * (10.toDouble().pow((l.size - index - 1).toDouble()))).toLong() }.sum()
 
-    fun inc(p: MutableList<Int>, cursor: Int) {
-        p[cursor] = 0
-        if (cursor == 0) {
-            p.add(0, 1)
-        } else {
-            p[cursor - 1] += 1
+    private fun increase(p: List<Int>, cursor: Int): List<Int> {
+        return when (cursor) {
+            0 -> (mutableListOf(1, 0).apply { this.addAll(p.subList(1, p.size)) }).toList()
+
+            else -> p.mapIndexed { index, i ->
+                when (index) {
+                    cursor -> 0
+                    cursor - 1 -> i + 1
+                    else -> i
+                }
+            }
         }
     }
 
